3c36ea9305afe60f06300c06a2ce3b0a03995781
Greg Neumann <greg@nsfwd.co.uk>
2015-10-26 10:51:53 +0000

    Version number 1.1 committed

--- CCCodePoint/Properties/AssemblyInfo.cs
+++ CCCodePoint/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CCCodePoint")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Ordnance Survey CodePoint Open Merge into Community Counts")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Next Step Forward Consultancy LTD")]
 [assembly: AssemblyProduct("CCCodePoint")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
